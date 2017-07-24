45bde7b5afbce313768919a2e52324ab6e3f3fcc
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-24 16:13:43 +1000

    Get the counter counting with constructors and class component.

--- my-first-gatsby-site/src/pages/index.js
+++ my-first-gatsby-site/src/pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import Link from 'gatsby-link'
 
-export default () =>
-	<div style={{ color: `tomato` }}>
-		<h1>Hello Gatsby!</h1>
-		<p>What a world!</p>
-		<img src="http://lorempixel.com/400/200/" />
-		<br />
-		<Link to="/my-second-gatsby-page/">Link</Link>
-	</div>
+class Counter extends React.Component {
+	constructor() {
+		super()
+		this.state = { count: 0 }
+	}
+	render() {
+		return (
+			<div>
+				<h1>Counter</h1>
+				<p>current counter: {this.state.count}</p>
+				<button onClick={() => this.setState({ count: this.state.count + 1 })}>plus</button>
+				<button onClick={() => this.setState({ count: this.state.count - 1 })}>minus</button>
+			</div>
+		)
+	}
+}
+
+export default Counter
