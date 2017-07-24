d49d868babbb88fcd5a64571edc4a3a71e71cd80
Syn-Ee Wong <synee.wong@ie.com.au>
2017-07-24 16:29:01 +1000

    Completing tutorial part one and publish site with surge.

--- my-first-gatsby-site/src/pages/index.js
+++ my-first-gatsby-site/src/pages/index.js
@@ -8,7 +8,7 @@ class Counter extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Counter</h1>
+				<h1>Counter Syn-Ee</h1>
 				<p>current counter: {this.state.count}</p>
 				<button onClick={() => this.setState({ count: this.state.count + 1 })}>plus</button>
 				<button onClick={() => this.setState({ count: this.state.count - 1 })}>minus</button>
